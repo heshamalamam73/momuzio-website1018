2637a664dcc6cdfb1de46ad2d4707010a67faacc
heshamalamam73 <heshamalamam73@gmail.com>
2018-10-03 21:54:11 +0100

    about hide

--- src/component/header/about.js
+++ src/component/header/about.js
@@ -5,11 +5,12 @@ const About = () =>{
     return (
         <div className='about'>
         <div className='left'>
+        <div className="contant">
         <h1>mahmoud hassan hamed okily </h1>
         <h4> full stack web developer</h4>
         <h4>heshamalamam73@gmail.com</h4>
+        </div>
         </div >
-        
         <div className='right'>
             <div className='myimg'>
                     <img src={myimg} alt='momuzio'/>
