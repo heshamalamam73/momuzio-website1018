97857540061c6913e69f3a11275586f793feaf8e
mahmoud hassan <mahmoudhassan@momuzio.local>
2018-10-20 15:39:47 +0200

    initial commit

--- src/component/header/portfolio.js
+++ src/component/header/portfolio.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Portfolio = () =>{
     return (
-        <div>
+        <div className='portofolia '>
+            <div className=''>
+                <h1>
+                    project 1 
+                </h1>
+                <p>
+                    loremipsom100
+                </p>
+                <a href='https://guarded-waters-59371.herokuapp.com/'>
+                    visit the project 
+                </a>
+                </div>
+                <div className='website'>
+                <iframe  src='https://guarded-waters-59371.herokuapp.com/'>
+                 </iframe>       
+                </div>
+        <hr></hr>
+           
+
+           
+           
+
         </div>
     )
 }
